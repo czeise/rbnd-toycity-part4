b02b6b275d5688dc3ccf99e0248f1d45a44fbcdf
Craig Zeise <craig.zeise@gmail.com>
2016-02-29 21:05:47 -0600

    Pass test_create_method_adds_to_database

--- lib/product.rb
+++ lib/product.rb
@@ -3,8 +3,9 @@ require_relative 'udacidata'
 class Product < Udacidata
   attr_reader :id, :price, :brand, :name
 
-  def initialize(opts={})
+  CSV_FILE = File.dirname(__FILE__) + '/../data/data.csv'
 
+  def initialize(opts = {})
     # Get last ID from the database if ID exists
     get_last_id
     # Set the ID if it was passed in, otherwise use last existing ID
@@ -17,14 +18,20 @@ class Product < Udacidata
     @price = opts[:price]
   end
 
+  def self.create(options = {})
+    product = new(options)
+    CSV.open(CSV_FILE, 'ab') do |csv|
+      csv << [product.id, product.brand, product.name, product.price]
+    end
+  end
+
   private
 
     # Reads the last line of the data file, and gets the id if one exists
     # If it exists, increment and use this value
     # Otherwise, use 0 as starting ID number
     def get_last_id
-      file = File.dirname(__FILE__) + "/../data/data.csv"
-      last_id = File.exist?(file) ? CSV.read(file).last[0].to_i + 1 : nil
+      last_id = File.exist?(CSV_FILE) ? CSV.read(CSV_FILE).last[0].to_i + 1 : nil
       @@count_class_instances = last_id || 0
     end
 
