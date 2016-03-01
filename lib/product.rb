e0a5c2f63c54be3ca21998ca6e00aa960f4972ca
Craig Zeise <craig.zeise@gmail.com>
2016-02-29 23:01:36 -0600

    Woops, was adding to the wrong class...

--- lib/product.rb
+++ lib/product.rb
@@ -3,9 +3,8 @@ require_relative 'udacidata'
 class Product < Udacidata
   attr_reader :id, :price, :brand, :name
 
-  CSV_FILE = File.dirname(__FILE__) + '/../data/data.csv'
+  def initialize(opts={})
 
-  def initialize(opts = {})
     # Get last ID from the database if ID exists
     get_last_id
     # Set the ID if it was passed in, otherwise use last existing ID
@@ -18,30 +17,14 @@ class Product < Udacidata
     @price = opts[:price]
   end
 
-  def self.create(options = {})
-    product = new(options)
-    CSV.open(CSV_FILE, 'ab') do |csv|
-      csv << [product.id, product.brand, product.name, product.price]
-    end
-    product
-  end
-
-  def self.all
-    products = []
-    CSV.foreach(CSV_FILE, headers: true) do |row|
-      products << new(id: row[:id], brand: row[:brand], name: row[:name],
-                      price: row[:price])
-    end
-    products
-  end
-
   private
 
     # Reads the last line of the data file, and gets the id if one exists
     # If it exists, increment and use this value
     # Otherwise, use 0 as starting ID number
     def get_last_id
-      last_id = File.exist?(CSV_FILE) ? CSV.read(CSV_FILE).last[0].to_i + 1 : nil
+      file = File.dirname(__FILE__) + "/../data/data.csv"
+      last_id = File.exist?(file) ? CSV.read(file).last[0].to_i + 1 : nil
       @@count_class_instances = last_id || 0
     end
 
--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -3,5 +3,22 @@ require_relative 'errors'
 require 'csv'
 
 class Udacidata
-  # Your code goes here!
+  CSV_FILE = File.dirname(__FILE__) + '/../data/data.csv'
+
+  def self.create(options = {})
+    product = Product.new(options)
+    CSV.open(CSV_FILE, 'ab') do |csv|
+      csv << [product.id, product.brand, product.name, product.price]
+    end
+    product
+  end
+
+  def self.all
+    products = []
+    CSV.foreach(CSV_FILE, headers: true) do |row|
+      products << new(id: row[:id], brand: row[:brand], name: row[:name],
+                      price: row[:price])
+    end
+    products
+  end
 end
