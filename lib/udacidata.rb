ed0663f5ed4f2b1e425cfa763bde0d8b10d763b0
Craig Zeise <craig.zeise@gmail.com>
2016-03-05 15:15:16 -0600

    Pass test_destroy_method_removes_product_from_database test

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -9,7 +9,7 @@ class Udacidata
   # #toycity4, if I'd like...I can go more in depth on this to make sure it's
   # the same product!
   def self.create(options = {})
-    product = Product.new(options)
+    product = new(options)
     CSV.open(CSV_FILE, 'ab') do |csv|
       csv << [product.id, product.brand, product.name, product.price]
     end
@@ -36,6 +36,20 @@ class Udacidata
   def self.find(id)
     all.find do |product|
       product.id == id
+    end
+  end
+
+  def self.destroy(id)
+    updated_products = all.delete_if { |product| product.id == id }
+    rewrite_csv(updated_products)
+  end
+
+  def self.rewrite_csv(products)
+    CSV.open(CSV_FILE, 'wb') do |csv|
+      csv << %w(id brand product price)
+      products.each do |product|
+        csv << [product.id, product.brand, product.name, product.price]
+      end
     end
   end
 end
--- tests/udacidata_test.rb
+++ tests/udacidata_test.rb
@@ -104,13 +104,13 @@ class TestUdacidata < MiniTest::Test
     assert_instance_of(Product, product)
   end
 
-  # def test_destroy_method_removes_product_from_database
-  #   before = CSV.read(@data_path).length
-  #   Product.destroy(2)
-  #   after = CSV.read(@data_path).length
-  #   assert_equal(before - 1, after)
-  # end
-  #
+  def test_destroy_method_removes_product_from_database
+    before = CSV.read(@data_path).length
+    Product.destroy(2)
+    after = CSV.read(@data_path).length
+    assert_equal(before - 1, after)
+  end
+
   # def test_destroy_method_returns_deleted_product
   #   product = Product.destroy(7)
   #   assert_equal(7, product.id)
