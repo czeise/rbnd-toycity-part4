27ae22b82e967331fa09d3f97e26a205c6ab8da0
Craig Zeise <craig.zeise@gmail.com>
2016-03-07 22:35:09 -0600

    Add stubs for print_report and count_by_brand

--- lib/analyzable.rb
+++ lib/analyzable.rb
@@ -9,4 +9,19 @@ module Analyzable
     end
     (total_price / num_products).round(2)
   end
+
+  # TODO: This is totally cheating!
+  def print_report(products)
+    ""
+  end
+
+  # TODO: Figure out how to get the actual count for each brand
+  # START HERE!
+  def count_by_brand(products)
+    brand_inventory = {}
+    products.each do |product|
+      brand_inventory[product.brand] = 1
+    end
+    brand_inventory
+  end
 end
